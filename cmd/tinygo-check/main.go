e9a8797df6e96ba30490a36080e4c9952072fb60
Răzvan Corneliu C.R. VILT <16434603+d3vi1@users.noreply.github.com.>
2026-02-01 22:04:55 +0200

    issue #17: add tinygo validation

---
+++ cmd/tinygo-check/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/d3vi1/helianthus-ebusgo/errors"
+	_ "github.com/d3vi1/helianthus-ebusgo/protocol"
+	_ "github.com/d3vi1/helianthus-ebusgo/transport"
+	_ "github.com/d3vi1/helianthus-ebusgo/types"
+)
+
+func main() {}
